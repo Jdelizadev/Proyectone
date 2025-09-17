36941ecd9a6d685278259801978f56cc6e6d3143
Jaime De Lizaola <jaimedelizaola@gmail.com>
2025-09-17 08:59:06 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+const PORT = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}/`)
+})
